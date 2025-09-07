aa0378dc4a15c2593266f47006b0997c98cce930
warisjan007 <vip.waris@gmail.com>
2025-09-07 13:16:35 +0200

    first commit

---
+++ dag-1.py
@@ -0,0 +1,27 @@
+
+#Soru 1:
+#x = 7 ve y = 3 değişkenlerini kullanarak z değişkenine bu ikisinin toplamını atayan Python kodunu yaz.
+x=7
+y=3
+z=x+y
+print("iki sayinin toplami : ",z)
+
+#Soru 2:
+#Kendi adını ve yaşını saklayacak iki değişken oluştur. Sonra bu değişkenleri kullanarak “Benim adım [adın] ve ben [yaşın] yaşındayım.” şeklinde bir mesajı ekrana yazdır.
+ad="Warisjan"
+yas=30
+print("Benim adım "+ ad + "ve ben " + str(yas) + "yaşındayım .")
+#Soru 3:
+#number = 10 değişkeni var. Bu değişkenin değerini 5 artıran bir kod satırı yaz.
+number = 10
+print("değişkenin değerini: " , number+5)
+#Soru 4:
+#Aşağıdaki değişken isimlerinden hangileri Python’da geçerlidir? Kendin deneyip sonucu yaz: _age, 2cool, my-var, name1
+#_age,ame1
+
+#Soru 5:
+#a = 4, b = 6 değişkenleri var. Bu iki değişkenin çarpımını c değişkenine atayıp ekrana yazdıran Python kodunu yaz.
+a = 4
+b = 6
+c = a*b
+print("iki sayinin carpimi : ", c)
---
+++ dag-2.py
@@ -0,0 +1,26 @@
+# Bir dizenin uzunluğunu almak için len()fonksiyonu kulliyoruz
+a="Warisjan Tohti"
+print(len(a))
+
+#tek tirnak ile cift tirnak arasinda fark yoktur
+print("Hello")
+print('Hello')
+
+#cift tirnak ile cift tirnak cakisiyorsa tek tirnak ile yazabiliriz
+print('it a "Dog"')
+
+#Çok satırlı bir dizeyi üç tırnak işareti kullanarak bir değişkene atayabilirsiniz:
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+# Veri tiplerinin degerini ogrenmek icin Type kullaniriz
+age = 40/4
+print(type(age))
+
+# mod almak icin
+print(10%4)
+# ussu almak icin
+print(2 ** 3)
