5be46056503202633c843a6da6ceedba5e3acf53
Uighur Robot <32806873+warisjan007@users.noreply.github.com>
2025-09-18 09:30:27 +0200

    Create quiz-1.py

---
+++ quiz-1.py
@@ -0,0 +1,47 @@
+# 1) Asagidaki string'in 5. harfini bir degiskene atayiniz
+my_string = "Python Ogreniyorum"
+#cevap
+#cevap
+# 2) Asagidaki String'in 5. ve 8. karakteri arasindaki tum harflerini yazdiriniz (5 ve 8 dahil)
+my_new_string = "ProgramlamayaMerhabaDedik"
+#cevap
+#cevap
+# 3) Asagidaki String'i kod ile tersten yazin
+my_last_string = "Afyonkarahisarlilastiramadiklarimizdanmisiniz"
+#cevap
+#cevap
+# 4) Asagidaki islemin sonucu hangi veri tipinde olacaktir?
+4 + 12.2 + 48
+
+#cevap
+#cevap
+# 5) Asagidaki islemin sonucu kactir?
+5 + 7 * 12
+
+#cevap
+#cevap
+# 6) Bu listeyi en az 2 farkli yoldan olusturunuz: [1,3,"a"]
+#cevap
+#cevap
+#cevap
+# 7) Asagidaki "b"'yi tek satirda aliniz:
+my_list = [3.14,4,[2,3,"b"],True]
+#cevap
+#cevap
+#cevap
+# 8) Asagidaki "a"'yi tek satirda aliniz:
+my_dictionary = {"key1":20.25, "kk2":[40,{"k21":"a"}]}
+#cevap
+#cevap
+# 9) Asagidaki liste set'e cevirilince hangi degerler icinde kalacaktir?
+my_list_to_be_set = [3,4,9,3,21,22,4,3,9,10,21,22]
+#cevap
+#cevap
+# 10) Asagidaki ifadenin sonucu ne olacaktir?
+x = 30 * 5 + 3
+y = 108 - 2 * 4
+x > y
+
+#cevap
+#cevap
+#cevap
