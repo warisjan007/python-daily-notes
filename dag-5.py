8b6f644bd0d587cd38d6d4f528f86e211b644b44
Uighur Robot <32806873+warisjan007@users.noreply.github.com>
2025-09-18 09:30:23 +0200

    Create dag-5.py

---
+++ dag-5.py
@@ -0,0 +1,28 @@
+                        #IF ELSE
+my_supperhero = "Batman"  
+if my_supperhero == "Katman": 
+ print ("True")
+else :
+ print ("False")
+
+                        # For
+my_list = [ 10 , 20 ,30 , 40 , 50 , 60 , 70 ] 
+for num in my_list :
+      print(num * 2)   
+# 6 ye tam bolunen sayilari yazdir :
+for num in my_list :
+   if num %6 == 0 :
+       print (num)
+
+for num in my_list :
+   print (num)
+   if num == 40 :
+      break
+   
+for num in my_list :
+   if num == 50 :
+      continue 
+   print(num)
+                        # While
+
+                        
